71bf0da4f9db3dc55b1cab4e18dd636ba22bdbd8
kienz7777 <kienvu7777@gmail.com>
2020-09-04 20:09:32 +0700

    Add TodoList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import TodoItem from './components/TodoItem';
+
+
 function App() {
+
+  const [todoItems, setTodoItems] = useState([
+    'Mua bim bim',
+    'Chó sửa gẩu',
+    'Đi đu đi đưa'
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+
+      {
+        
+        todoItems.map((item,index) => <TodoItem key={index} title={item}/>)
+      }
+
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TodoItem(props) {
+    return (
+        <div className="TodoItem">
+            <p>{props.title}</p>
+        </div>
+    )
+}
+
+
+export default TodoItem;
+
